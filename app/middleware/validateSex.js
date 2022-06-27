dd445faca44d5caae21612e664c5213ca06abd27
georgerdp-cic <george.crisan-cic@ibm.com>
2022-06-27 11:21:19 +0100

    [FIX][SEAS-387] Workaround sex page

--- app/middleware/validateSex.js
+++ app/middleware/validateSex.js
@@ -2,7 +2,10 @@
 
 
 function validateSex(req, res) {
-  if (req.body['sex']) {
+  if (req.headers.referer.includes('date-of-birth')) {
+    // This is a workaround to prevent the probme created by the fact that date of birth page makes a post request to this endpoint
+    res.render('citizen-application/sex', { validation: null });
+  } else if (req.body['sex']) {
     req.session.data.sex = req.body['sex'];
     res.redirect('national-insurance-number');
   } else {
