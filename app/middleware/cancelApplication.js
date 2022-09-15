88403437b5cb68cc937e2d7694e72ba52ba5d882
Johann Santos <Johann.Santos-CIC@ibm.com>
2022-09-15 12:05:09 +0100

    Updated Cancel Flow

--- app/middleware/cancelApplication.js
+++ app/middleware/cancelApplication.js
@@ -1,24 +1,22 @@
-
-
-
 function cancelApplication(req, res) {
-  let ref = req.session.data.app;
-   
-  for(app of req.session.data['applications']){
-      if(app.ref == ref){
-          app.status.id = '009';
-          app.status.text = 'CANCELLED'
-      }
-  }
+    let ref = req.session.data.app;
 
-  let selectedApplication = req.session.data['applications'].filter(value =>
-      value.ref == ref
-  )
-  
-  req.session.data.selectedApplication = selectedApplication;
-  res.redirect('application-cancelled');
+    for (app of req.session.data['applications']) {
+        if (app.ref == ref) {
+            app.status.id = '009';
+            app.status.text = 'CANCELLED';
+        }
+    }
+
+    let selectedApplication = req.session.data['applications'].filter(value => value.ref == ref);
+
+    req.session.data.selectedApplication = selectedApplication;
+
+    if (req.url == '/confirm-cancel') {
+        res.redirect('back');
+    } else {
+        res.redirect('application-cancelled');
+    }
 }
-
-
 
 exports.cancelApplication = cancelApplication;
