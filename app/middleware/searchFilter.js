b93caf353f9b3a37228897635b4dc7256367fa30
fizdoo <30404464+fizdoo@users.noreply.github.com>
2023-06-01 10:05:27 -0700

    CSig dashboard filter bug fix.

--- app/middleware/searchFilter.js
+++ app/middleware/searchFilter.js
@@ -1,6 +1,10 @@
 function searchFilter(req, res) {
     let filtered = req.session.data.filteredApplications.filter(app => {
-        return app.name.toLowerCase().includes(req.session.data.search.toLowerCase());
+        let result = false;
+        try {
+            result = app.name.toLowerCase().includes(req.session.data.search.toLowerCase());
+        } finally {}
+        return result;
     });
 
     req.session.data.filteredApplications = filtered;
